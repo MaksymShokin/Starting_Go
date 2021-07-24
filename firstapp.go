5daf56f0227639824b22b7fe504ef069820a9dd5
Maksym <maksym.sh@upplabs.com>
2021-07-24 14:33:38 +0300

    ints-and-strings

---
+++ firstapp.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var greetingText string
+	// greetingText = "Hello World"
+
+	var greetingText string = "Hello World"
+
+	// var greetingText = "Hello World"
+
+	// greetingText := "Hello World"
+	luckyNumber := 17
+
+	fmt.Println(greetingText)
+	fmt.Println(luckyNumber)
+
+}
+
+// run file go run first_app.go
