38544b00ce8f3f1051d90a09de712cd49bbe9485
Maksym <maksym.sh@upplabs.com>
2021-07-25 17:27:49 +0300

    runes-bytess

--- firstapp.go
+++ firstapp.go
@@ -21,11 +21,21 @@ func main() {
 	fmt.Println(superLuckyNumber)
 
 	var defaultFloat float64 = 1.12345678901234567890 // 1.1234567890123457
-	var smallFloat float32 = 1.12345678901234567890 // 1.1234568
+	var smallFloat float32 = 1.12345678901234567890   // 1.1234568
 
 	fmt.Println(defaultFloat)
 	fmt.Println(smallFloat)
 
+	var firstRune rune = '$'
+	fmt.Println(firstRune)
+	fmt.Println(string(firstRune))
+
+
+	var firstByte byte = 'a'
+	fmt.Println(firstByte)
+	fmt.Println(string(firstByte))
+
+	// var firstBool = false
 
 }
 
